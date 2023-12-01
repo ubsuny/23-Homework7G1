637da5038be8213549ac1bcd4db288858730958c
tirthbha <143649367+tirthbha@users.noreply.github.com>
2023-12-01 12:32:05 -0500

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,3 +1,8 @@
+import matplotlib.pyplot as plt 
+from mpl_toolkits.mplot3d import Axes3D  
+from mpl_toolkits.mplot3d.art3d import Line3DCollection  
+import scipy.optimize  
+
 from module import *
 # Define side lengths 'a' and 'b'
 a = 0.2 # length
