1bc029bcc963dac05ef3b222a600cb6448b24787
pratibha77118 <72980895+pratibha77118@users.noreply.github.com>
2023-11-30 23:28:44 -0500

    Check for corrections

--- nacl.py
+++ nacl.py
@@ -1,20 +1,17 @@
 """
 Defining Cluster class and related functions.
 """
-import numpy as np
 import itertools
-
+import numpy as np
 # Constants
 KE2 = 197 / 137  # eV-nm   Coulomb force charge
 ALPHA = 1.09e3  # eV      parameter of the model
 RHO = 0.0321    # nm      parameter of the model
 B = 1.0         # eV      regular
 C = 0.01
-
 def cp(l):
     """
     Generate combinations of elements from the input list l.
-    
     Parameters:
     - l: List of elements
     
@@ -22,30 +19,26 @@ def cp(l):
     - Combinations of elements taken two at a time.
     """
     return np.fromiter(itertools.chain(*itertools.combinations(l, 2)), dtype=int).reshape(-1, 2)
-
 class Cluster:
     """
     Class with positions and charges.
     """
-    def __init__(self, sodium_positions, chloride_positions):
+def __init__(self, na_positions, cl_positions):
         '''
         Initialize the Cluster object.
-        
         Parameters:
-        - sodium_positions: Positions of Na ions
-        - chloride_positions: Positions of Cl ions
+        - na_positions: Positions of Na ions
+        - cl_positions: Positions of Cl ions
         '''
-        self.positions = np.hstack((sodium_positions, chloride_positions))
-        self.charges = np.hstack([np.ones(sodium_positions.shape[0]), np.full(chloride_positions.shape[0], -1)])
+        self.positions = np.hstack((na_positions, cl_positions))
+        self.charges = np.hstack([np.ones(na_positions.shape[0]), np.full(cl_positions.shape[0], -1)])
         self.combs = cp(np.arange(self.charges.size))
         self.charge_prods = self.charges[self.combs][:, 0] * self.charges[self.combs][:, 1]
         self.rij = np.linalg.norm(self.positions[self.combs][:, 0] - self.positions[self.combs][:, 1], axis=1)
         self.v_ij_ = None
-        
-    def v_ij(self):
+def v_ij(self):
         '''
         Calculate numpy vectors for combinations.
-        
         Returns:
         - Numpy vector of potentials.
         '''
@@ -55,26 +48,21 @@ class Cluster:
         self.v_ij_[pos] = KE2 / self.rij[pos] + B * (C / self.rij[pos]) ** 12
         self.v_ij_[neg] = -KE2 / self.rij[neg] + ALPHA * np.exp(-self.rij[neg] / RHO) + B * (C / self.rij[neg]) ** 12
         return self.v_ij_
-
-    def v(self):
+def v(self):
         '''
         Total potential as sum of v_ij vector.
-        
         Returns:
         - Total potential
         '''
         return np.sum(self.v_ij())
-
-    def get_vals(self):
+def get_vals(self):
         '''
        Positions interpreted as a flat shape.
-        
         Returns:
         - Flattened array of positions.
         '''
         return np.reshape(self.positions, -1)
-
-    def set_vals(self, vals):
+def set_vals(self, vals):
         '''
         Set positions.
         Parameters:
@@ -82,18 +70,15 @@ class Cluster:
         '''
         self.positions = vals.reshape(self.positions.shape)
         self.rij = np.linalg.norm(self.positions[self.combs][:, 0] - self.positions[self.combs][:, 1], axis=1)
-
-    def __call__(self, vals):
+def __call__(self, vals):
         '''
         Function with scipy.optimize.minimize as call.
         Parameters:
         - vals: Positions
-        
         Returns:
         - Potential energy
         '''
         self.set_vals(vals)
         return self.v()
 
-# Run pylint on this modified code to check for further improvements.
-      
+#
